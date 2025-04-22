e78fc6971c574611cea90307cdfb391a82b6d9cb
Lina-Faikun <fajkunleonila@gmail.com>
2025-04-22 11:35:23 +0200

    fix loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,13 +1,12 @@
-import css from "./Loader.module.css";
+import { RingLoader } from 'react-spinners';
+import css from './Loader.module.css';
 
 export default function Loader() {
   return (
-    <>
-      <RingLoader
-        size={60}
-        color="#fff"
-        className={css.loader}
-      />
-    </>
+    <RingLoader
+      size={60}
+      color="#fff"
+      className={css.loader}
+    />
   );
 }
